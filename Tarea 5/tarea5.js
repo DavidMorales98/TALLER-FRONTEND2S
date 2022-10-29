9202ed17e5975dfbc28948927d8d1ed7db4c8731
DavidMorales98 <david.morales.p@usach.cl>
2022-10-29 12:31:50 -0300

    Añade de otra forma la función de la pregunta 3

--- Tarea 5/tarea5.js
+++ Tarea 5/tarea5.js
@@ -1,6 +1,17 @@
 //Tarea numero 5.
 //David Morales P. 19881480-6
 
+//1.- Secuencia de fibonacci
+function fibonacci(n){
+    //condicional de termino
+    if (n === 1 || n===0){
+        return n;
+    }
+    //llamado recursivo
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+console.log(fibonacci(5))
 //2.- Del siguiente arreglo de strings retornar otro arreglo con todo a mayúsculas.
 
 let pokeones = [
@@ -9,16 +20,20 @@ let pokeones = [
     'Bulbasaur',
     'Squirtle'
 ];
-
+//Entrada: Arreglo de strings
+//Salida: Strings en mayusculas
+//Funcion: Convierte los strings con minusculas a mayusculas
 function mayusculas (arreglo){
+    //Iterar arreglo
     for (let i in arreglo){
+        //Almacenar en la misma posicion el string en mayuscula
         arreglo[i]=arreglo[i].toUpperCase();
-        console.log(arreglo[i])
     }
+    //Retorno del arreglo actualizado
     return arreglo;
 };
 
-mayusculas(pokeones);
+console.log(mayusculas(pokeones));
 
 //3.- Del siguiente arreglo de objetos, retornar otro arreglo con los pokemones tipo fuego.
 let pokemones = [
@@ -51,16 +66,25 @@ let pokemones = [
         tipo: 'Fuego'
     }
 ]
-
+//Entrada: Arreglo de pokemones
+//Salida: Arreglo de pokemones tipo fuego
+//Funcion: Seleciona los pokemones de tipo fuego
 function tipoFuego (arreglo){
+    //arreglo vacio
     let fuego = [];
-    for (let i in arreglo){
-        if (arreglo[i].tipo === 'Fuego'){
-            console.log(arreglo[i]);
-            fuego.push(arreglo[i].nombre);
+    //iteracion de los elementos del arreglo
+    for (let pokemon of arreglo){
+        //condicional del enunciado
+        if (pokemon.tipo === 'Fuego'){
+            //almacenar en el arreglo vacio
+            fuego.push(pokemon.nombre);
         }
     }
+    //retorno del arreglo
     return fuego;
 }
+console.log(tipoFuego(pokemones));
 
-tipoFuego(pokemones);
+//Mediante metodo filter
+const pokemonesFuego = pokemones.filter(pokemon => pokemon.tipo === 'Fuego');
+console.log(pokemonesFuego)
