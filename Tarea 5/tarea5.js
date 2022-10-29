ba3a63c34ca11794cda72cc78ad675a6b3b38082
DavidMorales98 <david.morales.p@usach.cl>
2022-10-29 11:24:02 -0300

    Se envian archivos de la tarea 3 a la carpeta origen

--- Tarea 5/tarea5.js
+++ Tarea 5/tarea5.js
@@ -1,2 +1,21 @@
 //Tarea numero 5.
 //David Morales P. 19881480-6
+
+//2.- Del siguiente arreglo de strings retornar otro arreglo con todo a mayúsculas.
+
+let pokeones = [
+    'Pikachu',
+    'Charmander',
+    'Bulbasaur',
+    'Squirtle'
+];
+
+function mayusculas (arreglo){
+    for (i in arreglo){
+        arreglo[i].toUpperCase();
+        console.log(arreglo[i])
+    }
+    return arreglo;
+};
+
+mayusculas(pokeones);
