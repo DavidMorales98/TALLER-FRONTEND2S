80f711243536aee15459b975767beba77db9fbc0
DavidMorales98 <david.morales.p@usach.cl>
2022-10-29 11:49:43 -0300

    Se termina pregunta 2 y 3

--- Tarea 5/tarea5.js
+++ Tarea 5/tarea5.js
@@ -11,11 +11,56 @@ let pokeones = [
 ];
 
 function mayusculas (arreglo){
-    for (i in arreglo){
-        arreglo[i].toUpperCase();
+    for (let i in arreglo){
+        arreglo[i]=arreglo[i].toUpperCase();
         console.log(arreglo[i])
     }
     return arreglo;
 };
 
 mayusculas(pokeones);
+
+//3.- Del siguiente arreglo de objetos, retornar otro arreglo con los pokemones tipo fuego.
+let pokemones = [
+    {
+        nombre: 'Pikachu',
+        tipo: 'Electrico'
+    },
+    {
+        nombre: 'Charmander',
+        tipo: 'Fuego'
+    },
+    {
+        nombre: 'Bulbasaur',
+        tipo: 'Planta'
+    },
+    {
+        nombre: 'Squirtle',
+        tipo: 'Agua'
+    },
+    {
+        nombre: 'Charmeleon',
+        tipo: 'Fuego'
+    },
+    {
+        nombre: ' Weedle', 
+        tipo: 'Bicho'
+    },
+    {
+        nombre: 'Charizard',
+        tipo: 'Fuego'
+    }
+]
+
+function tipoFuego (arreglo){
+    let fuego = [];
+    for (let i in arreglo){
+        if (arreglo[i].tipo === 'Fuego'){
+            console.log(arreglo[i]);
+            fuego.push(arreglo[i].nombre);
+        }
+    }
+    return fuego;
+}
+
+tipoFuego(pokemones);
