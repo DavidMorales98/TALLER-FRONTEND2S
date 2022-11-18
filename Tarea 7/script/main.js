b5841ab6d1462301b974fb4910e307a58debbc30
DavidMorales98 <david.morales.p@usach.cl>
2022-11-18 17:25:25 -0300

    Avance tarea7

--- Tarea 7/script/main.js
+++ Tarea 7/script/main.js
@@ -1,44 +1,83 @@
-let base_url = 'https://pokeapi.co/api/v2/pokemon/';
-let pokemones = undefined;
-
-axios.get(base_url)
-.then(function(response)
-{
-    pokemones = response.data.results;
-    console.log(pokemones);
-    document.getElementById('js-nombre1').innerHTML = pokemones[0].name;
-    document.getElementById('js-nombre2').innerHTML = pokemones[1].name;
-    document.getElementById('js-nombre3').innerHTML = pokemones[2].name;
-    document.getElementById('js-nombre4').innerHTML = pokemones[3].name;
-    document.getElementById('js-nombre5').innerHTML = pokemones[4].name;
-    document.getElementById('js-nombre6').innerHTML = pokemones[5].name;
-    document.getElementById('js-nombre7').innerHTML = pokemones[6].name;
-    document.getElementById('js-nombre8').innerHTML = pokemones[7].name;
-    document.getElementById('js-nombre9').innerHTML = pokemones[8].name;
-    document.getElementById('js-nombre10').innerHTML = pokemones[9].name;
-
-    document.getElementById('js-nombre11').innerHTML = pokemones[10].name;
-    document.getElementById('js-nombre12').innerHTML = pokemones[11].name;
-    document.getElementById('js-nombre13').innerHTML = pokemones[12].name;
-    document.getElementById('js-nombre14').innerHTML = pokemones[13].name;
-    document.getElementById('js-nombre15').innerHTML = pokemones[14].name;
-    document.getElementById('js-nombre16').innerHTML = pokemones[15].name;
-    document.getElementById('js-nombre17').innerHTML = pokemones[16].name;
-    document.getElementById('js-nombre18').innerHTML = pokemones[17].name;
-    document.getElementById('js-nombre19').innerHTML = pokemones[18].name;
-    document.getElementById('js-nombre20').innerHTML = pokemones[19].name;
-})
-.catch(function(error){
-    pokemones = 'No data';
-})
-.then(function(){
-
-});
-var img=document.getElementById('NaveRickyMorty');
+var img = document.getElementById('NaveRickyMorty');
 // Establecer el evento de clic (cuando se mueve el mouse) para lograr efectos dinámicos
-document.onmousemove=function (e) {
+document.onmousemove = function (e) {
     // Establece la posición de la imagen igual a la posición del mouse (puedes sumar o restar la distancia apropiadamente)
-    img.style.left=e.pageX-80 + 'px';
-    img.style.top=e.pageY-80 + 'px';
+    img.style.left = e.pageX - 80 + 'px';
+    img.style.top = e.pageY - 80 + 'px';
 }
 
+$(document).ready(function(){
+    $("button").click(function(){
+      $("#container-portals").toggle();
+    });
+  });
+
+/* 
+$(document).ready(function () {
+    $(".btn1").click(function () {
+        $(document.getElementById('container-portals')).hide();
+    });
+    $(".btn2").click(function () {
+        $(document.getElementById('container-portals')).show();
+    });
+});
+*/
+//POO
+//URL API
+let base_url = 'https://rickandmortyapi.com/api/character';
+//Lista personajes
+let personajes = undefined;
+
+//Clase personajes
+class Personaje {
+    //Constructor atributos
+    constructor(Nombre, Especie, Imagen) {
+        this.nombre = Nombre;
+        this.especie = Especie;
+        this.imagen = Imagen;
+    }
+    //Metodos
+    //Getter
+    get Info() {
+        return `Personaje ${this.nombre}, especie ${this.especie} su imagen $(this.imagen)`;
+    }
+    //Setter
+    set cambiarNombre(nuevoNombre) {
+        this.nombre = nuevoNombre;
+    }
+
+    set cambiarEspecie(nuevaEspecie) {
+        this.especie = nuevaEspecie;
+    }
+    set carmbiarImagen(nuevaImagen) {
+        this.imagen = nuevaImagen;
+    }
+}
+
+// Petición al servidor de Rick y Morty para obtener los personajes
+axios.get(base_url)
+  .then(function (response) {
+    // Funcionó
+    // Mostrar el mensaje de alerta de exito
+    personajes = response.data.results;
+    console.log(personajes);
+    personajes.map((item) => {
+      let personaje = new Personaje(item.name,item.species,item.image)
+      api.innerHTML += `<div class="container-img"><h1>${personaje.nombre}</h1><p>${personaje.especie}</p><img src="${personaje.imagen}"/></div>`;
+    });
+  })
+  .catch(function (error) {
+    // No funcionó
+    // Mostrar el mensaje de alerta de error
+    //console.log(error);
+
+  })
+  .then(function () {
+    // Siempre se ejecuta
+    // Mostrar el mensaje de alerta de cargando
+
+  });
+
+  // function show(params){
+  //   insertar html
+  // }
